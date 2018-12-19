6b36f738d222280083fa2824c61e612de41644c4
Jonathan Burstein <jonathan@jargon.com>
2018-12-19 12:55:48 -0800

    Flesh out actions-on-google ResponseFactory
    ResponseFactory now contains methods for all core response types that
    have user-visible string content. Certain types (namely table) would
    require multiple calls into the resource factory to construct, depending
    on the specifics of the desired response. This can be improved upon on future
    versions by checking the values for various fields (columns, rows, etc.) to
    see if they're Jargon variants and rendering as needed.

--- packages/actions-on-google/lib/common/responseFactory.ts
+++ packages/actions-on-google/lib/common/responseFactory.ts
@@ -12,7 +12,7 @@
  */
 
 import { RenderItem, ResourceManager } from '@jargon/sdk-core'
-import { BasicCard, SimpleResponse, GoogleActionsV2UiElementsImage, GoogleActionsV2UiElementsButton, GoogleActionsV2UiElementsBasicCardImageDisplayOptions, BasicCardOptions, SimpleResponseOptions } from 'actions-on-google'
+import { BasicCard, BasicCardOptions, BrowseCarouselItem, BrowseCarouselItemOptions, Button, ButtonOptions, GoogleActionsV2UiElementsBasicCardImageDisplayOptions, GoogleActionsV2UiElementsButton, GoogleActionsV2UiElementsImage, GoogleActionsV2UiElementsOpenUrlAction, GoogleActionsV2UiElementsTableCardColumnPropertiesHorizontalAlignment, Image, ImageOptions, LinkOutSuggestion, LinkOutSuggestionOptions, MediaObject, MediaObjectOptions, SimpleResponse, SimpleResponseOptions, Suggestions, Table, TableColumn, TableOptions, TableRow } from 'actions-on-google'
 
 export interface JBasicCardOptions {
   title?: RenderItem
@@ -22,17 +22,89 @@ export interface JBasicCardOptions {
   buttons?: GoogleActionsV2UiElementsButton | GoogleActionsV2UiElementsButton[]
   display?: GoogleActionsV2UiElementsBasicCardImageDisplayOptions
 }
-const jbcFields = ['title', 'subtitle', 'text']
+const jBasicCardFields = ['title', 'subtitle', 'text']
+
+export interface JBrowseCarouselItemOptions {
+  title: RenderItem
+  url: string
+  description?: RenderItem
+  footer?: RenderItem
+  image?: GoogleActionsV2UiElementsImage
+}
+const jBrowseCarouselItemFields = ['title', 'description', 'footer']
+
+export interface JButtonOptions {
+  title: RenderItem
+  url?: string
+  action?: GoogleActionsV2UiElementsOpenUrlAction
+}
+const jButtonFields = ['title']
+
+export interface JImageOptions {
+  url: string
+  alt: RenderItem
+  height?: number
+  width?: number
+}
+const jImageFields = ['alt']
+
+export interface JLinkOutSuggestionOptions {
+  name: RenderItem
+  url: string
+}
+const jLinkOutSuggestionFields = ['name']
+
+export interface JMediaObjectOptions {
+  url: string
+  description?: RenderItem
+  name?: RenderItem
+  icon?: GoogleActionsV2UiElementsImage
+  image?: GoogleActionsV2UiElementsImage
+}
+const jMediaObjectFields = ['description', 'name']
 
 export interface JSimpleResponseOptions {
   speech: RenderItem
   text?: RenderItem
 }
-const jsrFields = ['speech', 'text']
+const jSimpleResponseFields = ['speech', 'text']
+
+export interface JTableOptions {
+  title?: RenderItem
+  subtitle?: RenderItem
+  image?: GoogleActionsV2UiElementsImage
+  columns?: (TableColumn | string)[] | number
+  columnProperties?: (TableColumn | string)[]
+  rows: (TableRow | string[])[]
+  dividers?: boolean
+  buttons?: GoogleActionsV2UiElementsButton | GoogleActionsV2UiElementsButton[]
+}
+const jTableOptionsFields = ['title', 'subtitle']
+
+export interface JTableColumn {
+  header?: RenderItem
+  horizontalAlignment?: GoogleActionsV2UiElementsTableCardColumnPropertiesHorizontalAlignment
+  align?: GoogleActionsV2UiElementsTableCardColumnPropertiesHorizontalAlignment
+}
+const jTableColumnFields = ['header']
+
+export interface JTableRow {
+  cells?: RenderItem[]
+  dividerAfter?: boolean
+}
 
 export interface ResponseFactory {
   basicCard (options: JBasicCardOptions): Promise<BasicCard>
+  browseCarouselItem (options: JBrowseCarouselItemOptions): Promise<BrowseCarouselItem>
+  button (options: JButtonOptions): Promise<Button>
+  image (options: JImageOptions): Promise<Image>
+  linkOutSuggestion (options: JLinkOutSuggestionOptions): Promise<LinkOutSuggestion>
+  mediaObject (options: JMediaObjectOptions): Promise<MediaObject>
   simple (options: JSimpleResponseOptions | RenderItem): Promise<SimpleResponse>
+  suggestions (...suggestions: RenderItem[]): Promise<Suggestions>
+  table (options: JTableOptions): Promise<Table>
+  tableColumn (column: JTableColumn): Promise<TableColumn>
+  tableRow (row: JTableRow): Promise<TableRow>
 }
 
 export class CommonResponseFactory implements ResponseFactory {
@@ -40,8 +112,33 @@ export class CommonResponseFactory implements ResponseFactory {
   constructor (private _rm: ResourceManager) {}
 
   async basicCard (options: JBasicCardOptions): Promise<BasicCard> {
-    const card: BasicCardOptions = await this._render(options, jbcFields)
+    const card: BasicCardOptions = await this._render(options, jBasicCardFields)
     return new BasicCard(card)
+  }
+
+  async browseCarouselItem (options: JBrowseCarouselItemOptions): Promise<BrowseCarouselItem> {
+    const bci: BrowseCarouselItemOptions = await this._render(options, jBrowseCarouselItemFields)
+    return new BrowseCarouselItem(bci)
+  }
+
+  async button (options: JButtonOptions): Promise<Button> {
+    const button: ButtonOptions = await this._render(options, jButtonFields)
+    return new Button(button)
+  }
+
+  async image (options: JImageOptions): Promise<Image> {
+    const image: ImageOptions = await this._render(options, jImageFields)
+    return new Image(image)
+  }
+
+  async linkOutSuggestion (options: JLinkOutSuggestionOptions): Promise<LinkOutSuggestion> {
+    const los: LinkOutSuggestionOptions = await this._render(options, jLinkOutSuggestionFields)
+    return new LinkOutSuggestion(los)
+  }
+
+  async mediaObject (options: JMediaObjectOptions): Promise<MediaObject> {
+    const mos: MediaObjectOptions = await this._render(options, jMediaObjectFields)
+    return new MediaObject(mos)
   }
 
   async simple (options: JSimpleResponseOptions | RenderItem): Promise<SimpleResponse> {
@@ -49,14 +146,38 @@ export class CommonResponseFactory implements ResponseFactory {
       return new SimpleResponse(await this._rm.render(options))
     }
 
-    const sro: SimpleResponseOptions = await this._render(options, jsrFields)
+    const sro: SimpleResponseOptions = await this._render(options, jSimpleResponseFields)
     return new SimpleResponse(sro)
   }
 
-  protected async _render<J, G> (jopts: J, fields: string[]): Promise<G> {
+  async suggestions (...suggestions: RenderItem[]): Promise<Suggestions> {
+    const rendered = await this._rm.renderBatch(suggestions)
+    return new Suggestions(rendered)
+  }
+
+  async table (options: JTableOptions): Promise<Table> {
+    const to: TableOptions = await this._render(options, jTableOptionsFields)
+    return new Table(to)
+  }
+
+  async tableColumn (column: JTableColumn): Promise<TableColumn> {
+    const c: TableColumn = await this._render(column, jTableColumnFields)
+    return c
+  }
+
+  async tableRow (row: JTableRow): Promise<TableRow> {
+    let cells: string[] | undefined
+    if (row.cells) {
+      cells = await this._rm.renderBatch(row.cells)
+    }
+
+    return Object.assign({}, row, { cells: cells })
+  }
+
+  protected async _render<J, G> (jOpts: J, fields: string[]): Promise<G> {
     let ris: RenderItem[] = []
     for (const f of fields) {
-      const val = jopts[f]
+      const val = jOpts[f]
       val && ris.push(val)
     }
     const rendered = await this._rm.renderBatch(ris)
@@ -64,12 +185,12 @@ export class CommonResponseFactory implements ResponseFactory {
     let pg: Partial<G> = {}
     let i = 0
     for (const f of fields) {
-      if (jopts[f]) {
+      if (jOpts[f]) {
         pg[f] = rendered[i++]
       }
     }
 
-    pg = Object.assign({}, jopts, pg)
+    pg = Object.assign({}, jOpts, pg)
     return pg as G
   }
 }
--- packages/actions-on-google/tst/common/responseFactory.spec.ts
+++ packages/actions-on-google/tst/common/responseFactory.spec.ts
@@ -11,49 +11,172 @@
  * permissions and limitations under the License.
  */
 
-import { expect } from 'chai'
 import { DefaultResourceManagerFactory, DefaultResourceManagerOptions, ri } from '@jargon/sdk-core'
-import { CommonResponseFactory, JBasicCardOptions, JSimpleResponseOptions } from '../../lib/common'
+import { expect } from 'chai'
+import { CommonResponseFactory, JBasicCardOptions, JBrowseCarouselItemOptions, JButtonOptions, JImageOptions, JLinkOutSuggestionOptions, JMediaObjectOptions, JSimpleResponseOptions, JTableColumn, JTableOptions, JTableRow } from '../../lib/common'
+
+/* tslint:disable:no-unused-expression */
 
 const rm = new DefaultResourceManagerFactory(DefaultResourceManagerOptions).forLocale('en-US')
 const rf = new CommonResponseFactory(rm)
 
+const riText = ri('TEXT')
+const riTitle = ri('TITLE')
+
+const cropped = 'CROPPED'
+const text = 'text'
+const title = 'title'
+
 it('Creates the expected BasicCard with title and text', async () => {
   const opts: JBasicCardOptions = {
-    title: ri('TITLE'),
-    text: ri('TEXT'),
+    title: riTitle,
+    text: riText,
     buttons: [],
-    display: 'CROPPED'
+    display: cropped
   }
 
   const card = await rf.basicCard(opts)
-  expect(card.title).equals('title')
-  expect(card.formattedText).equals('text')
+  expect(card.title).equals(title)
+  expect(card.formattedText).equals(text)
   expect(card.buttons).length(0)
-  expect(card.imageDisplayOptions).equals('CROPPED')
+  expect(card.imageDisplayOptions).equals(cropped)
+  expect(card.image).to.be.undefined
+})
+
+it('Creates a browse carousel item', async () => {
+  const url = 'https://www.example.com/an_item'
+  const opts: JBrowseCarouselItemOptions = {
+    title: riTitle,
+    url: url,
+    description: riText,
+    footer: riText
+  }
+
+  const bci = await rf.browseCarouselItem(opts)
+  expect(bci.openUrlAction!.url).equals(url)
+  expect(bci.description).equals(text)
+  expect(bci.footer).equals(text)
+  expect(bci.title).equals(title)
+})
+
+it('Creates a button', async () => {
+  const url = 'https://www.example.com'
+  const opts: JButtonOptions = {
+    title: riTitle,
+    url: url
+  }
+
+  const button = await rf.button(opts)
+  expect(button.title).equals(title)
+  expect(button.openUrlAction!.url).equals(url)
+})
+
+it('Creates an image', async () => {
+  const url = 'https://www.example.com/image.png'
+  const opts: JImageOptions = {
+    url: url,
+    alt: riText
+  }
+
+  const img = await rf.image(opts)
+  expect(img.accessibilityText).equals(text)
+  expect(img.url).equals(url)
+})
+
+it('Creates a link out suggestion', async () => {
+  const url = 'https://www.example.com'
+  const opts: JLinkOutSuggestionOptions = {
+    url: url,
+    name: riTitle
+  }
+
+  const los = await rf.linkOutSuggestion(opts)
+  expect(los.destinationName).equals(title)
+  expect(los.url).equals(url)
+})
+
+it('Creates a media object', async () => {
+  const url = 'https://www.example.com/sounds.mp4'
+  const opts: JMediaObjectOptions = {
+    name: riTitle,
+    description: riText,
+    url: url
+  }
+
+  const ms = await rf.mediaObject(opts)
+  expect(ms.contentUrl).equals(url)
+  expect(ms.description).equals(text)
+  expect(ms.name).equals(title)
 })
 
 it('Creates a simple response from a single render item', async () => {
-  const simple = await rf.simple(ri('TITLE'))
-  expect(simple.textToSpeech).equals('title')
+  const simple = await rf.simple(riTitle)
+  expect(simple.textToSpeech).equals(title)
   expect(simple.displayText).equals(undefined)
 })
 
 it('Creates a simple response from options without display text', async () => {
   const opt: JSimpleResponseOptions = {
-    speech: ri('TITLE')
+    speech: riTitle
   }
   const simple = await rf.simple(opt)
-  expect(simple.textToSpeech).equals('title')
+  expect(simple.textToSpeech).equals(title)
   expect(simple.displayText).equals(undefined)
 })
 
 it('Creates a simple response from options with display text', async () => {
   const opt: JSimpleResponseOptions = {
-    speech: ri('TITLE'),
-    text: ri('TEXT')
+    speech: riTitle,
+    text: riText
   }
   const simple = await rf.simple(opt)
-  expect(simple.textToSpeech).equals('title')
-  expect(simple.displayText).equals('text')
+  expect(simple.textToSpeech).equals(title)
+  expect(simple.displayText).equals(text)
+})
+
+it('Creates a suggestion with a single member', async () => {
+  const s = await rf.suggestions(riText)
+  expect(s.suggestions).length(1)
+  expect(s.suggestions[0].title).equals(text)
+})
+
+it('Creates a suggestion with a multiple members', async () => {
+  const s = await rf.suggestions(riText, riTitle)
+  expect(s.suggestions).length(2)
+  expect(s.suggestions[0].title).equals(text)
+  expect(s.suggestions[1].title).equals(title)
+})
+
+it('Creates a table', async () => {
+  const to: JTableOptions = {
+    title: riTitle,
+    subtitle: riText,
+    rows: []
+  }
+
+  const table = await rf.table(to)
+  expect(table.title!).equals(title)
+  expect(table.subtitle!).equals(text)
+})
+
+it('Creates a table column', async () => {
+  const tc: JTableColumn = {
+    header: riTitle
+  }
+
+  const column = await rf.tableColumn(tc)
+  expect(column.header!).equals(title)
+})
+
+it('Creates a table row', async () => {
+  const jtr: JTableRow = {
+    cells: [riText, riTitle],
+    dividerAfter: true
+  }
+
+  const tr = await rf.tableRow(jtr)
+  expect(tr.cells!).length(2)
+  expect(tr.cells![0].toString()).equals(text)
+  expect(tr.cells![1].toString()).equals(title)
+  expect(tr.dividerAfter).to.be.true
 })
