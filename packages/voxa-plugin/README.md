6d9ecc6a0917e9412d417fa5e4c077dad31211cc
Jonathan Burstein <jonathan@jargon.com>
2019-02-28 16:43:16 -0800

    Merge of dev/voxa into mainline (squashed).
    Includes the following core changes:
    
    commit 9f1bb608f14b6841b559bb7750483ee3958faa41
    Author: Jonathan Burstein <jonathan@jargon.com>
    
        Support returing render items from voxa variables
    
        This builds on the just added support for nested render items in
        ResourceManager.renderObject. With this change all variable functions
        are always called during rendering, even if the variable won't be used
        by the specified resource(s). While this is different behavior than
        the standard Voxa renderer, it's also something we'd need to do when
        performing remote rendering.
    
        Variable function calls aren't memoized, but it's not clear if we'd gain
        much by doing so as those calls should be fast and idempotent.
    
    commit abb25145783ea3b6f540ad457243bedbfb044c15
    Author: Jonathan Burstein <jonathan@jargon.com>
    
        Initial Voxa plugin implementation
    
        The 'JargonVoxa' function handles creating the VoxaApp, configured with the
        Jargon renderer and plugin (request handler). The Jargon request handler takes
        care of creating a ResourceManager for the request's locale, and places it onto
        the IVoxaEvent for the request. JargonRenderer uses the resource manager to render
        the resources for the response. If desired options for that render operation can
        be placed onto the event.
    
        Includes tests for basic functionality. Those tests do not yet cover all of the
        different directive and transition types, in particular transitions that specify
        a reply object, or a view containing multiple directives.
    
        All rendering is using ResourceManager.renderObject, so variations aren't yet
        supported; that will happen for a 1.0 release.

---
+++ packages/voxa-plugin/README.md
@@ -0,0 +1,3 @@
+# Jargon SDK for the Voxa Framework
+
+This is a preview release of the Jargon SDK plugin for the [Voxa Framework](https://github.com/VoxaAI/voxa).
---
+++ packages/voxa-plugin/tst/renderer.spec.ts
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2019 Jargon, Inc. or its affiliates. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import { expect } from 'chai'
+import { DefaultResourceManagerFactory, ri } from '../lib'
+import { JargonRenderer } from '../lib/renderer'
+import { IVoxaEvent, AlexaEvent, AlexaPlatform, VoxaApp } from 'voxa'
+import { AlexaRequestBuilder } from './tools'
+
+const arb = new AlexaRequestBuilder()
+const rmf = new DefaultResourceManagerFactory({})
+const app = new VoxaApp({ views: {} })
+
+function makeVars (basic: string, plural: number) {
+  return {
+    basicParam: () => basic,
+    pluralParam: () => plural
+  }
+}
+
+function renderer (variables?: any): JargonRenderer {
+  const config = {
+    variables: variables,
+    views: {}
+  }
+  return new JargonRenderer(config)
+}
+
+function makeEvent (locale: string = 'en-US'): IVoxaEvent {
+  const r = arb.getIntentRequest('FakeIntent')
+  const jrm = rmf.forLocale(locale)
+  const e = new AlexaEvent(r)
+  e.platform = new AlexaPlatform(app)
+
+  return {
+    ...e,
+    jrm,
+    jargonResourceManager: jrm
+  }
+}
+
+it('Renders a simple string', async () => {
+  const r = renderer()
+  const s = await r.renderPath('simple', makeEvent())
+  expect(s).equals('simple')
+})
+
+it('Renders using the correct locale', async () => {
+  const r = renderer()
+  const s = await r.renderPath('simple', makeEvent('de-DE'))
+  expect(s).equals('de simple')
+})
+
+it('Renders a basic parameter correctly', async () => {
+  const param = 'This is my param'
+  const vars = makeVars(param, 0)
+  const r = renderer(vars)
+  const s = await r.renderPath('basicParam', makeEvent())
+  expect(s).equals(`basicParam: ${param}`)
+})
+
+it('Uses passed in parameters instead of ones from config', async () => {
+  const param = 'This is my param'
+  const vars = makeVars('Not the param we should use', 0)
+  const r = renderer(vars)
+  const s = await r.renderPath('basicParam', makeEvent(), { basicParam: param })
+  expect(s).equals(`basicParam: ${param}`)
+})
+
+it('Renders the correct plural case for 0', async () => {
+  const vars = makeVars('', 0)
+  const r = renderer(vars)
+  const s = await r.renderPath('pluralParam', makeEvent())
+  expect(s).equals('zero')
+})
+
+it('Renders the correct plural case for 1', async () => {
+  const vars = makeVars('fish', 1)
+  const r = renderer(vars)
+  const s = await r.renderPath('pluralParam', makeEvent())
+  expect(s).equals('One fish')
+})
+
+it('Renders the correct plural case for other', async () => {
+  const vars = makeVars('', 17)
+  const r = renderer(vars)
+  const s = await r.renderPath('pluralParam', makeEvent())
+  expect(s).equals('17')
+})
+
+it('Correctly passes the voxa event to variables when rendering', async () => {
+  const id = 'UserID'
+  const vars = {
+    basicParam: (e: IVoxaEvent) => e.user.userId,
+    pluralParam: () => 0
+  }
+  const r = renderer(vars)
+  const e = makeEvent()
+  e.user.userId = id
+  const s = await r.renderPath('basicParam', e)
+  expect(s).equals(`basicParam: ${id}`)
+})
+
+it('Correctly handles nested render items via variables', async () => {
+  const param = 'param'
+  const vars = {
+    basicParam: () => ri('sub', { param }),
+    pluralParam: () => 2
+  }
+  const r = renderer(vars)
+  const s = await r.renderPath('basicParam', makeEvent())
+  expect(s).equals(`basicParam: ${param}`)
+})
+
+it('Returns the platform-specific content if present', async () => {
+  const r = renderer()
+  const s: any = await r.renderPath('platformSpecific', makeEvent())
+  expect(s.message).equals('alexa-specific message')
+})
